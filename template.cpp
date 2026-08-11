67d1016542661fd10c1325dcba92f9060b12601a
sohakulkarni20 <thesohak@gmail.com>
2026-08-11 12:12:37 +0530

    new changes made

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    cout << "Enter size of array: ";
+    int size;
+    cin >> size;
+    vector<int> arr;
+    int v;
+    cout << "Enter values in sorted manner:\n";
+    for (int i = 0; i < size; i++) {
+        cout << "At index " << i << " : ";
+        cin >> v;
+        arr.push_back(v);
+    }
+    cout << "Original vector: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
