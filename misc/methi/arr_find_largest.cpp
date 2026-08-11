c637e03246e5bd3700ca06066341ad9142a278fd
sohakulkarni20 <thesohak@gmail.com>
2026-08-11 12:21:41 +0530

    Update arr_find_largest.cpp

--- misc/methi/arr_find_largest.cpp
+++ misc/methi/arr_find_largest.cpp
@@ -2,56 +2,10 @@
 using namespace std;
 
 
-/*
-use two pointers
-ptr1 = 0th index
-ptr2 = 1th index
 
-
-
-//akh to fixed tha na 0th pr
-//but waha rukha h na ptr2 ko jaha ptr1 ki value equal na ho ptr2 
-// a= 0th index
-m = first index
-int temp = 0;
-while(m<arr.size()){
-if(a == m){
-    m++;
-}
-else{
-    a++;
-    temp = arr[a];
-    arr[a] = arr[m];
-    arr[m] = temp;
-    m++;
-    }
-}
-
-//neend ari
-//joro wali
-    
-
-agar 1 == 2 false hua, then we gotta swap
-use liye m++ counter se bahar ana padega na
-to swap kab krenge?
- 
-
-
-
-
-
-*/
 int MAX=100;
 void remove_duplicates(int arr[]){
-    // [1,1,2,3,3]
-    //agar ese kiya, new array banaya, usme visited element daale
-    //and everytime we gotta add an element 
-    //it will check if the element is present previously in visited ones
-    //aur agar present hoga, to skip krdenge print krte time
-    //or else add in arr
-    //answer me array kyu chahiye😭😭😭 
-    //correct
-    //wahi wo complexity badhti jayegi
+    
     //show
     int count=0;
     for(int i=0; i<5; i++){
@@ -71,8 +25,7 @@ void remove_duplicates(int arr[]){
         
     }
 }
-//karu run?
-//bhai kya tha wo
+
 int main()
 {
     cout<<"array:"<<endl;
